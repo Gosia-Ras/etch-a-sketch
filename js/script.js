45e0937b360dea258c475c19f37b5276fe76cf96
Gosia Hildebrand <ras.malgorzata@gmail.com>
2023-11-14 11:15:31 +0100

    Added functionality to the new grid button

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
-let container = document.querySelector(".gridContainer");
+const container = document.querySelector(".gridContainer");
+const newGridButton = document.querySelector(".newGrid");
 
-for (var i = 0; i < 16; i++) {
-  let gridItem = document.createElement("div");
-  gridItem.className = "item";
-  gridItem.id = i + 1;
-  container.appendChild(gridItem);
+function createGrid(size) {
+  container.innerHTML = "";
 
-  gridItem.addEventListener("mouseover", (e) => {
-    gridItem.style.backgroundColor = "pink";
-  });
+  for (let i = 0; i < size * size; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.className = "item";
+    gridItem.id = i + 1;
+
+    gridItem.addEventListener("mouseover", () => {
+      gridItem.style.backgroundColor = "pink";
+    });
+
+    container.appendChild(gridItem);
+  }
+
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 }
+
+function isValidGridSize(size) {
+  return size && !isNaN(size) && size > 0 && size <= 100;
+}
+
+newGridButton.addEventListener("click", function () {
+  let gridSize = prompt("Enter the number of squares per side (max: 100):");
+  if (isValidGridSize(gridSize)) {
+    createGrid(parseInt(gridSize));
+  } else {
+    alert("Invalid input. Please enter a number between 1 and 100.");
+  }
+});
+
+// Initial grid creation
+createGrid(16);
