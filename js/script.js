4ca88da4591b507e55bba17602d15d0470236369
Gosia Hildebrand <ras.malgorzata@gmail.com>
2023-12-21 16:28:00 +0100

    Added random color generator

--- js/script.js
+++ js/script.js
@@ -1,23 +1,70 @@
 const container = document.querySelector(".gridContainer");
 const newGridButton = document.querySelector(".newGrid");
+const colorPickerContainer = document.querySelector(".colorPicker");
+const randomColor = document.querySelector("#random");
+
+// Set the default color
+let currentColor = "black";
+let isRandomColor = false; // Flag to indicate if random color mode is active
 
 function createGrid(size) {
   container.innerHTML = "";
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
   for (let i = 0; i < size * size; i++) {
     const gridItem = document.createElement("div");
     gridItem.className = "item";
     gridItem.id = i + 1;
-
-    gridItem.addEventListener("mouseover", () => {
-      gridItem.style.backgroundColor = "pink";
-    });
-
     container.appendChild(gridItem);
   }
-
-  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 }
+
+container.addEventListener(
+  "mouseenter",
+  (event) => {
+    if (event.target.className === "item") {
+      if (isRandomColor) {
+        currentColor =
+          "rgb(" +
+          Math.floor(Math.random() * 256) +
+          "," +
+          Math.floor(Math.random() * 256) +
+          "," +
+          Math.floor(Math.random() * 256) +
+          ")";
+      }
+      event.target.style.backgroundColor = currentColor;
+    }
+  },
+  true
+);
+
+randomColor.addEventListener("click", () => {
+  isRandomColor = true;
+});
+
+const colorArray = [
+  "red",
+  "blue",
+  "orange",
+  "yellow",
+  "black",
+  "white",
+  "pink",
+  "purple",
+  "green",
+];
+
+colorArray.forEach((color) => {
+  const colorButton = document.createElement("button");
+  colorButton.style.backgroundColor = color;
+  colorPickerContainer.appendChild(colorButton);
+
+  colorButton.addEventListener("click", () => {
+    currentColor = color;
+    isRandomColor = false;
+  });
+});
 
 function isValidGridSize(size) {
   return size && !isNaN(size) && size > 0 && size <= 100;
